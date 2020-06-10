e586061b2e3771489f1d7ff2ca0c5589b6b9ba37
brian <mutismusembi@gmail.com>
2020-06-10 12:10:33 +0300

    Auomaically sed whatsapp messages Whatsaapp Heroku

--- honey.py
+++ honey.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client 
  
 account_sid = 'AC708c100bd6e92969a434fc101ea4d91f' 
-auth_token = 'def213828937ec289d87ff4e117b1bf6' 
+auth_token = '[Auth_Token]' 
 client = Client(account_sid, auth_token) 
 def send():
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
                               body='We are experimenting',      
-                              to='whatsapp:+254797578553' 
+                              to='whatsapp:+2547' 
                           ) 
  
     print(message.sid)
